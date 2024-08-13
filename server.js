233d7ecb1a6ac4816a9bb3e0cf9ffba664fc4691
millenniumSonule <anshumansonuley@gmail.com>
2024-08-14 01:07:58 +0530

    .

--- server.js
+++ server.js
@@ -1,14 +1,17 @@
-const http = require('http');
+http = require('node:http');
+listener = function (request, response) {
+   // Send the HTTP header 
+   // HTTP Status: 200 : OK
+   // Content Type: text/html
+   response.writeHead(200, {'Content-Type': 'text/html'});
+  
+   // Send the response body as "Hello World"
+   response.end('<h2 style="text-align: center;">Hello World</h2>');
+};
 
-const hostname = '127.0.0.1';
-const port = 3000;
+server = http.createServer(listener);
+server.listen(3000);
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello, World!\n');
-});
+// Console will print the message
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+console.log('Server running at http://127.0.0.1:3000/');
